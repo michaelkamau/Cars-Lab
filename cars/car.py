13a0996ddbf3c5a0d39e6a61577d2fbb8bca44b4
Michael Kamau <tokamau@gmail.com>
2016-10-10 07:50:44 +0300

    Add attribute num_of_doors
    Its value depends on the name of car

--- cars/car.py
+++ cars/car.py
@@ -2,3 +2,4 @@ class Car:
     def __init__(self, name='General', model = 'GM'):
         self.name = name
         self.model = model
+        self.num_of_doors = 2 if self.name == 'Porshe' or self.name == 'Koenigsegg' else 4
