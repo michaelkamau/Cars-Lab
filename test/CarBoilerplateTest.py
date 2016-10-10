41c54b9d8f846188161e974e4b38f58eb42fe1e9
Michael Kamau <tokamau@gmail.com>
2016-10-10 07:12:56 +0300

    Add test for Car class instantiation

---
+++ test/CarBoilerplateTest.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class CarClassTest(unittest.TestCase):
+    """docstring for CarClassTest"""
+
+    def test_car_instance(self):
+        honda = Car('Honda')
+        self.assertIsInstance(honda, Car, msg='The object should be an instance of the `Car` class')
+
+
+if __name__ == '__main__':
+    unittest.main()
