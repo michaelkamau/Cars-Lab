624007dadc00236793bb0789ff3b84f150de38aa
Michael Kamau <tokamau@gmail.com>
2016-10-10 08:16:11 +0300

    Add attribute speed, initial value zero

--- cars/car.py
+++ cars/car.py
@@ -4,6 +4,7 @@ class Car:
         self.model = model
         self.num_of_doors = 2 if self.name == 'Porshe' or self.name == 'Koenigsegg' else 4
         self.num_of_wheels = 8 if type == 'trailer' else 4
+        self.speed = 0
 
     def is_saloon(self):
         return True if self.num_of_wheels == 4 else False
