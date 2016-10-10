b63f303010bf95df723f403bbee9dc82f7e89097
Michael Kamau <tokamau@gmail.com>
2016-10-10 07:58:11 +0300

    Add attribute type defaults to 'saloon'

--- cars/car.py
+++ cars/car.py
@@ -1,5 +1,5 @@
 class Car:
-    def __init__(self, name='General', model = 'GM'):
+    def __init__(self, name='General', model = 'GM', type='saloon'):
         self.name = name
         self.model = model
         self.num_of_doors = 2 if self.name == 'Porshe' or self.name == 'Koenigsegg' else 4
