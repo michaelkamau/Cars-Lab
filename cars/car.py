55e8b0f71752d8db84a5050a828475f13db90dbd
Michael Kamau <tokamau@gmail.com>
2016-10-10 08:00:14 +0300

    Add attribute num_of_wheel depending on type

--- cars/car.py
+++ cars/car.py
@@ -3,3 +3,4 @@ class Car:
         self.name = name
         self.model = model
         self.num_of_doors = 2 if self.name == 'Porshe' or self.name == 'Koenigsegg' else 4
+        self.num_of_wheels = 8 if type == 'trailer' else 4
