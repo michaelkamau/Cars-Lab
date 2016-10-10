cca377f3985905db78a70aca0833412a1e06ce4f
Michael Kamau <tokamau@gmail.com>
2016-10-10 07:22:45 +0300

    Add test for Car class object creation

--- test/CarBoilerplateTest.py
+++ test/CarBoilerplateTest.py
@@ -1,5 +1,5 @@
 import unittest
-
+from cars.car import Car
 
 class CarClassTest(unittest.TestCase):
     """docstring for CarClassTest"""
@@ -7,6 +7,10 @@ class CarClassTest(unittest.TestCase):
     def test_car_instance(self):
         honda = Car('Honda')
         self.assertIsInstance(honda, Car, msg='The object should be an instance of the `Car` class')
+
+    def test_object_type(self):
+        honda = Car('Honda')
+        self.assertTrue((type(honda) is Car), msg='The object should be a type of `Car`')
 
 
 if __name__ == '__main__':
